cf392c51fa0f24f54d993fa0f2c400f3f8cb0ddb
Paul Fleury <paul.fleuryp@gmail.com>
2015-09-21 18:38:31 +0200

    Added today extension

--- iLamps Widget/TodayViewController.swift
+++ iLamps Widget/TodayViewController.swift
@@ -1,12 +1,13 @@
 //
-//  ViewController.swift
-//  iLamps
+//  TodayViewController.swift
+//  iLamps Widget
 //
-//  Created by PAPAV Fleury on 10/06/2015.
+//  Created by Paul Fleury on 21/09/15.
 //  Copyright © 2015 Paul Fleury. All rights reserved.
 //
 
 import UIKit
+import NotificationCenter
 
 struct Actions {
     static let onDesktop = 1
@@ -23,40 +24,30 @@ var outputStream: NSOutputStream!
 var readStream:  Unmanaged<CFReadStream>?
 var writeStream: Unmanaged<CFWriteStream>?
 
-class ViewController: UIViewController {
-    
-    @IBAction func onDesktop(sender: AnyObject) {
-        connectToServer()
-        sendData(Actions.onDesktop)
-    }
-    
-    @IBAction func offDesktop(sender: AnyObject) {
-        connectToServer()
-        sendData(Actions.offDesktop)
-    }
-    
-    @IBAction func onBed(sender: AnyObject) {
-        connectToServer()
-        sendData(Actions.onBed)
-    }
-    
-    @IBAction func offBed(sender: AnyObject) {
-        connectToServer()
-        sendData(Actions.offBed)
-    }
-    
+class TodayViewController: UIViewController, NCWidgetProviding {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
+        // Do any additional setup after loading the view from its nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.NewData)
+    }
+    @IBAction func onDesktop(sender: AnyObject) {
+        connectToServer()
+        sendData(Actions.onDesktop)
     }
     
     func connectToServer() {
@@ -76,15 +67,11 @@ class ViewController: UIViewController {
         let data = signal.dataUsingEncoding(NSUTF8StringEncoding)
         if outputStream.write(UnsafePointer<UInt8>(data!.bytes), maxLength: data!.length) == -1 {
             let error = outputStream.streamError?.description
-            if #available(iOS 8.0, *) {
-                let errorPopup: UIAlertController = UIAlertController(title: "Error", message: "An error as occured \(error)", preferredStyle: .Alert)
-                let okAction: UIAlertAction = UIAlertAction(title: "Ok", style: .Cancel) { action -> Void in }
-                errorPopup.addAction(okAction)
-                presentViewController(errorPopup, animated: true, completion: nil)
-            }
+            let errorPopup: UIAlertController = UIAlertController(title: "Error", message: "An error as occured \(error)", preferredStyle: .Alert)
+            let okAction: UIAlertAction = UIAlertAction(title: "Ok", style: .Cancel) { action -> Void in }
+            errorPopup.addAction(okAction)
+            presentViewController(errorPopup, animated: true, completion: nil)
         }
     }
     
-    
 }
-
